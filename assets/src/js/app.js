5deb6134043168fb8b93591f417521e7f02952de
Patrik Elias <patrik.elias@elitesolutions.sk>
2022-09-13 09:09:00 +0200

    Detail trip in progress

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -13,12 +13,32 @@ $(document).ready(function() {
         
     $('.home-slider').slick({
         dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: false 
+    });
+ 
+    $('.tolding-about__wrapper').slick({
+        dots: true,
         infinite: true,
         speed: 300,
         slidesToShow: 1,
         adaptiveHeight: true
     });
- 
+
+    $('.trip__slider').slick({
+        dots: false,
+        infinite: true,
+        arrows: true,
+        autoplay: true,
+        speed: 3000,
+        slidesToShow: 1,
+        adaptiveHeight: true
+    });
+
+
+     
    
 
 });
